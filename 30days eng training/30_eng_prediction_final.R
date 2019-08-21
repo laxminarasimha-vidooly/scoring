390e8d31863a4965fae6912af841cc2e7c2b9f3e
laxminarasimha-vidooly <54302572+laxminarasimha-vidooly@users.noreply.github.com>
2019-08-21 16:40:04 +0530

    Update 30_eng_prediction_final.R

--- 30days eng training/30_eng_prediction_final.R
+++ 30days eng training/30_eng_prediction_final.R
@@ -1,15 +1,15 @@
 ###########################
 #LifetimeSubscriber prediction
 ###########################
-#install.packages("randomForest")
+install.packages("randomForest")
 require(randomForest)
 require(dplyr)
-setwd("E:/Laxmi_Rnd/My Laptop/Work/Scoring_algo/Raw dumps for scoring training/30days eng training")
+setwd("/Scoring_algo/Raw dumps for scoring training/30days eng training")
 ##########################
 #preparing test data
 ##########################
 
-data2<-read.table("E:\\Laxmi_Rnd\\My Laptop\\Work\\Scoring_algo\\consolidated.csv", header=T, stringsAsFactors = F, sep = ",")
+data2<-read.table("\\Scoring_algo\\consolidated.csv", header=T, stringsAsFactors = F, sep = ",")
 head(data2)
 data3_3<-data2[data2$last_30_eng==0,]
 
@@ -38,7 +38,7 @@ test$upload_range<-as.factor(test$upload_range)
 #####################################################
 #preparing training data
 #####################################################
-overall_data<-read.csv("E:\\Laxmi_Rnd\\My Laptop\\Work\\Scoring_algo\\Raw dumps for scoring training\\30days eng training\\New_training_data.csv",header = T, stringsAsFactors = F)
+overall_data<-read.csv("Scoring_algo\\Raw dumps for scoring training\\30days eng training\\New_training_data.csv",header = T, stringsAsFactors = F)
 overall_data1<-overall_data[c(2:10)]
 overall_data1<-overall_data1[overall_data1$last_30_views>0,]
 
@@ -154,7 +154,9 @@ levels(test$category)<-levels(train$category)
 levels(test$view_range)<-levels(train$View_range)
 levels(test$upload_range)<-levels(train$upload_range)
 colnames(test)<-c("subscriber_lifetime","views_lifetime","life_uploads", "last_30_views","category","View_range","upload_range")
-
+##################################################
+##prediction on test data##
+##################################################
 model.rf.predict1<-predict(model.rf,newdata=test,type='response', predict.all=TRUE,  
                            norm.votes=TRUE)
 
